0216decf7a801fb1113bfe0570a65db5c35f200f
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-03-11 19:40:53 +0100

    Update longfile.cs

--- longfile.cs
+++ longfile.cs
@@ -5,7 +5,7 @@ namespace HelloWorld
     public class CalculateClass
     {
         int l1, l2, l3, l4, l5, l6, l7, l8, l9;
-        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
         {
             l1 = v1;
             l2 = v2;
